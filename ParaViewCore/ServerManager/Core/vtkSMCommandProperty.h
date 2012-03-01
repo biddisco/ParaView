aa0315e684fdda60274787aac4b27b521d9398af
John Biddiscombe <biddisco@cscs.ch>
2012-03-01 22:11:49 +0100

    Add vtkSMCommandProperty for mapping commands to buttons
    This adds support for creating a button which can be used to
    execute a command when clicked.
    
    Command property is a stateless property which is currently
    used in computational steering code.
    
    Conflicts:
    	ParaViewCore/ServerManager/CMakeLists.txt
    	Qt/Core/pqPropertyLinks.cxx

---
+++ ParaViewCore/ServerManager/Core/vtkSMCommandProperty.h
@@ -0,0 +1,44 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMCommandProperty.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkSMCommandProperty - abstract superclass for all vector properties
+// .SECTION Description
+// vtkSMCommandProperty defines an interface common to all vector properties
+// as well as some common settings. A vector property contains a list
+// of values passed to one or more invocations of a command. How the
+// values are distributed to the different invocations is controlled
+// by several parameters.
+
+#ifndef __vtkSMCommandProperty_h
+#define __vtkSMCommandProperty_h
+
+#include "vtkSMProperty.h"
+
+class VTK_EXPORT vtkSMCommandProperty : public vtkSMProperty
+{
+public:
+  static vtkSMCommandProperty* New();
+  vtkTypeMacro(vtkSMCommandProperty, vtkSMProperty);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+protected:
+  vtkSMCommandProperty();
+  ~vtkSMCommandProperty();
+
+private:
+  vtkSMCommandProperty(const vtkSMCommandProperty&); // Not implemented
+  void operator=(const vtkSMCommandProperty&); // Not implemented
+};
+
+#endif
