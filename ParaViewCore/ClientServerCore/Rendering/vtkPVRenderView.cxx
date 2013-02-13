05090daca90e577edbe3e848bc006da5aba066f4
John Biddiscombe <biddisco@cscs.ch>
2013-02-13 20:04:13 +0100

    Pass PkdTree to ordered compositing, disable default redistribution
    Change-Id: I9315eb3f553fc97452c2bb47b817790f402cbcf9

--- ParaViewCore/ClientServerCore/Rendering/vtkPVRenderView.cxx
+++ ParaViewCore/ClientServerCore/Rendering/vtkPVRenderView.cxx
@@ -1322,7 +1322,7 @@ void vtkPVRenderView::SetStreamable(
 void vtkPVRenderView::SetOrderedCompositingInformation(
   vtkInformation* info, vtkPVDataRepresentation* repr,
   vtkExtentTranslator* translator,
-  const int whole_extents[6], const double origin[3], const double spacing[3])
+  const int whole_extents[6], const double origin[3], const double spacing[3], vtkPKdTree *tree)
 {
   vtkPVRenderView* view = vtkPVRenderView::SafeDownCast(info->Get(VIEW()));
   if (!view)
@@ -1331,7 +1331,7 @@ void vtkPVRenderView::SetOrderedCompositingInformation(
     return;
     }
   view->GetDeliveryManager()->SetOrderedCompositingInformation(
-    repr, translator, whole_extents, origin, spacing);
+    repr, translator, whole_extents, origin, spacing, tree);
 }
 
 //----------------------------------------------------------------------------
