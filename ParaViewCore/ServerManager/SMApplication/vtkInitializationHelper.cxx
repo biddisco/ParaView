26eeb00b2194b94eab17a0cdc9a3d1557b45dc97
John Biddiscombe <biddisco@cscs.ch>
2014-11-13 14:10:35 +0100

    Add mpi_comm param to process module init so we can initialize from an already split communicator

--- ParaViewCore/ServerManager/SMApplication/vtkInitializationHelper.cxx
+++ ParaViewCore/ServerManager/SMApplication/vtkInitializationHelper.cxx
@@ -165,7 +165,7 @@ void vtkInitializationHelper::Initialize(const char* executable,
 
 //----------------------------------------------------------------------------
 void vtkInitializationHelper::Initialize(int argc, char**argv,
-  int type, vtkPVOptions* options)
+  int type, vtkPVOptions* options, int mpi_comm)
 {
   if (vtkProcessModule::GetProcessModule())
     {
@@ -184,7 +184,7 @@ void vtkInitializationHelper::Initialize(int argc, char**argv,
   GOOGLE_PROTOBUF_VERIFY_VERSION;
 
   vtkProcessModule::Initialize(
-    static_cast<vtkProcessModule::ProcessTypes>(type), argc, argv);
+    static_cast<vtkProcessModule::ProcessTypes>(type), argc, argv, mpi_comm);
 
   vtksys_ios::ostringstream sscerr;
   if (argv && !options->Parse(argc, argv) )
